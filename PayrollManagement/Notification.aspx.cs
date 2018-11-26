e2ec8ff1a912925c5ada9c83e5797e497e936bac
FuadMannan <fuad.mannan@gmail.com>
2018-11-26 17:44:22 -0500

    Added email settings to web config, and added functionality to Notification page

--- PayrollManagement/Notification.aspx.cs
+++ PayrollManagement/Notification.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -16,7 +17,32 @@ namespace PayrollManagement
 
         protected void confirm_Click(object sender, EventArgs e)
         {
-
+            SmtpClient smtpClient = new SmtpClient();
+            MailMessage message = new MailMessage();
+            try
+            {
+                // Prepare receiving email address
+                message.To.Add(new MailAddress(
+                    toAddressTxtBox.Text, "To You"));
+                // Prepare sending email address
+                message.From = new MailAddress(
+                    "testmail.fuad@gmail.com", "Libra Pay");
+                message.Subject = subjectTxtBox.Text;
+                message.Body = messageTxtBox.Text +
+                    "<br/><br/>------------------------------------<br/>";
+                message.IsBodyHtml = true;
+                // Set server details
+                smtpClient.Host = "smtp.gmail.com";
+                // Send the email
+                smtpClient.Send(message);
+                // Inform the user
+                resultLabel.Text = "Email sent.";
+            }
+            catch
+            {
+                // Display error message
+                resultLabel.Text = "Coudn't send the message!";
+            }
         }
 
         protected void clear_Click(object sender, EventArgs e)
--- PayrollManagement/Notification.aspx.designer.cs
+++ PayrollManagement/Notification.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace PayrollManagement {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl outerFrame;
         
         /// <summary>
+        /// resultLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label resultLabel;
+        
+        /// <summary>
         /// fromAddressTxtBox control.
         /// </summary>
         /// <remarks>
